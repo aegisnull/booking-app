ba9f584060ff6c8bf9981fdde3f2f8ec6ab1152e
Luis Tellez <luis@luistellez.com>
2023-02-25 14:39:56 -0800

    style: add styling to Navbar component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
+import styles from "../../styles/Navbar.module.scss";
+
 function Navbar() {
   return (
     <>
-      <h1>Navbar</h1>
-      <h2>Navigation</h2>
+      <div className={styles.navbar}>
+        <div className={styles.navbar__container}>
+          <div className={styles.navbar__logo}>LuisBooking.com</div>
+          <nav className={styles.navbar__nav}>
+            <button className={styles.navbar__nav_button}>Register</button>
+            <button className={styles.navbar__nav_button}>Login</button>
+          </nav>
+        </div>
+      </div>
     </>
   );
 }
