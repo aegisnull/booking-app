8a6dd93baacab95b50da672d452fc4cbdc82d56e
Luis Tellez <luis@luistellez.com>
2023-02-25 14:43:27 -0800

    feat: init Header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../../styles/Header.module.scss";
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>My App</h1>
+    </div>
+  );
+}
+
+export default Header;
