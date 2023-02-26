effe25f15cafb0cb1b93933c2d52aa73f0862cde
Luis Tellez <luis@luistellez.com>
2023-02-25 19:40:16 -0800

    feat: add fields for Search component

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -1,10 +1,39 @@
+import {
+  faBed,
+  faCalendarDays,
+  faPerson,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../../styles/Search.module.scss";
 
 function Search() {
   return (
     <>
       <div className={styles.search}>
-        <h2>Search</h2>
+        <div className={styles.search__field}>
+          <FontAwesomeIcon icon={faBed} />
+          <input
+            type="text"
+            placeholder="Where are you going?"
+            className={styles.search__input}
+          />
+        </div>
+        <div className={styles.search__field}>
+          <FontAwesomeIcon icon={faCalendarDays} />
+          <input
+            type="text"
+            placeholder="Check in - Check out"
+            className={styles.search__input}
+          />
+        </div>
+        <div className={styles.search__field}>
+          <FontAwesomeIcon icon={faPerson} />
+          <input
+            type="text"
+            placeholder="2 adults - 0 children - 1 room"
+            className={styles.search__input}
+          />
+        </div>
       </div>
     </>
   );
